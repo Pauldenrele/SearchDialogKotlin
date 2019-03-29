9c34e2b33379bc4801d0e4981a94a20a789e940e
PAULI <adenreleademola@yahoo.com>
2019-03-29 15:50:26 +0100

    Adding a custom library and creating a searchModel class

---
+++ app/src/main/java/com/example/searchdialogkotlin/searchModel.kt
@@ -0,0 +1,10 @@
+package com.example.searchdialogkotlin
+
+import ir.mirrajabi.searchdialog.core.Searchable
+
+class searchModel (private var mTitle:String?):Searchable{
+    override fun getTitle(): String {
+        return mTitle!!
+    }
+
+}
